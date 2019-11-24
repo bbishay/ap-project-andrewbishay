81dfc74cd055322cc89c281e0a2594df2f9b9393
Andrew Bishay <50244538+bbishay@users.noreply.github.com>
2019-11-23 22:22:46 -0500

    figured out array; need to make 2D array

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-function checkInput(ob){
+function checkInput(input){
     const invalidChars = /[^1-9]/gi
-            if(invalidChars.test(ob.value)) {
-                    ob.value = ob.value.replace(invalidChars,"");
+            if(invalidChars.test(input.value)) {
+                    input.value = input.value.replace(invalidChars,'');
           }
 };
 //restricts typeable characters in input to only numerical values
 
+
+//obtain input values and create two-dimensional array
 document.querySelector('.submit').addEventListener('click', function(){
     //console.log('test1');
-
-    console.log(document.querySelectorAll('.square-small').value);
-    
+    let values = [];
+    for (i = 1; i < 82; i++){
+        values.push(Number(document.querySelector(`.square-small${i}`).value));
+    }
+    //console.log(values);
 });
+
+
+
+
+//test each square from 1-9 against each row
+
+
+
+
+
+//test each square from 1-9 against each column
+
+
+
+
+
+//test each square from 1-9 against each large square
+
+
+//replace each empty value with a valid value
+
+//display
